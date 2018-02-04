79af1ae0493878cf96b08f11e63dd0c093b64e26
yamada.takumi <riorim5313@outlook.jp>
2018-02-04 17:47:37 +0900

    First Commit!!

---
+++ Ghost/Assets/Script/ghostController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ghostController : MonoBehaviour {
+
+	public GameObject controlObject;	// 憑依対象を保持するオブジェクト
+	public bool isPossessing = false;			// 憑依中フラグ
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// 憑依(操作対象を切り替える)
+	public void Possession (GameObject GObj) {
+		
+		// 現在のControlの子オブジェクト(Controllable)を解除
+		foreach (Transform child in controlObject.transform) {
+			
+			if (child.gameObject.tag == "Controllable") {
+				
+				child.gameObject.GetComponent<BoxCollider> ().enabled = true;	// BoxColliderを再起動
+				child.parent = null;	// 子オブジェクトを解除
+			}
+		}
+
+		// Controlの子オブジェクトとして登録
+		GObj.transform.parent = controlObject.transform;
+
+		// 憑依対象の位置に自身を転送
+		this.transform.position = GObj.transform.position;
+
+		// 憑依対象のBoxColliderを取得
+		BoxCollider boxcol = GObj.GetComponent<BoxCollider> ();
+
+		// 憑依対象の大きさを調べて位置(憑依後の高さ)やcolliderの大きさを反映
+		float GObj_Height = GObj.transform.localScale.y * boxcol.size.y;
+
+		// 高さの反映とローカル位置の固定
+		GObj.transform.localPosition = new Vector3 (0, GObj_Height * 0.5f, 0);
+
+		// 憑依対象のBoxColliderを一時停止
+		boxcol.enabled = false;
+
+		// rigidbodyに移動制限を掛ける(ローカル位置の固定)
+		GObj.GetComponent<Rigidbody> ().constraints = RigidbodyConstraints.FreezePosition;
+
+		// 憑依中フラグを立てる
+		isPossessing = true;
+	}
+
+	// 離脱(操作対象から離れる)
+	public void Disengagement(){
+		foreach (Transform child in controlObject.transform) {
+
+			if (child.gameObject.tag == "Controllable") {
+
+				child.gameObject.GetComponent<BoxCollider> ().enabled = true;	// BoxColliderを再起動
+				child.parent = null;	// 子オブジェクトを解除
+			}
+		}
+	}
+}
---
+++ Ghost/Assets/Script/someGaze.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class someGaze : MonoBehaviour {
+
+	public Camera camera;
+	public float rayDist = 10.0f;
+	public float rayRadius = 2.0f;
+	public ghostController GhostCtrl;
+
+	// Use this for initialization
+	void Start () {
+		GhostCtrl = GetComponent<ghostController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		Ray ray = camera.ViewportPointToRay (new Vector3(0.5f, 0.5f, 0));
+
+		Debug.DrawRay (ray.origin, ray.direction * rayDist, Color.black);				// cameraの向き
+
+		// マウス離上時
+		if (Input.GetMouseButtonUp (1)) {
+			// cameraが何かを注視している
+			if (Physics.SphereCast (ray, rayRadius, out hit, rayDist)) {
+				//Debug.Log (hit.collider.gameObject.name);
+
+				// 対象のTagがControllableのとき
+				if (hit.collider.tag == "Controllable") {
+
+					// 憑依
+					GhostCtrl.Possession (hit.collider.gameObject);
+				}
+			// 何も見ていない
+			} else {
+				// 離脱
+				GhostCtrl.Disengagement();
+			}
+		}
+	}
+}
