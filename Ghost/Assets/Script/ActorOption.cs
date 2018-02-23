ab5664332a057399d083811e7dbb214266222811
yamada.takumi <riorim5313@outlook.jp>
2018-02-23 16:51:37 +0900

    Add ActorOption

---
+++ Ghost/Assets/Script/ActorOption.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActorOption : MonoBehaviour {
+
+	// Actor Type List
+	public enum TypeList{
+		None = 0,
+		Ghost,
+		Human,
+		Bat,
+		Golem,
+		Shaman
+	}
+	// Property
+	public TypeList Type{ get; set; }
+
+	// Method : Initializing
+	public void Initialize(TypeList SetType){
+		Type = SetType;		// Defining ActorType
+	}
+
+	// Method : Setting of what the Camera can see
+	public virtual void SetViewable (Camera camera){
+		camera.cullingMask = (1 << LayerMask.NameToLayer ("Psychic"));
+		camera.cullingMask = (1 << LayerMask.NameToLayer ("Physics"));
+	}
+}
