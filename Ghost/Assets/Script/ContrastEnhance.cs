de24e9d45549ed73fe795cfcf79329d1ab9d5d6d
yamada.takumi <riorim5313@outlook.jp>
2018-04-02 23:43:06 +0900

    add Contract Enhance

---
+++ Ghost/Assets/Script/ContrastEnhance.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContrastEnhance : MonoBehaviour {
+
+	public Shader shader;
+	public Material mat;
+
+	void Start()
+	{
+		this.mat = new Material (this.shader);
+	}
+
+	void OnRenderImage(RenderTexture src, RenderTexture dest )
+	{
+		Graphics.Blit (src, dest, this.mat);
+	}
+}
